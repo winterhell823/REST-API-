4b260decb81d98ea57114ed9146d2d98342fa2c3
winterhell823 <101448374+winterhell823@users.noreply.github.com>
2026-07-04 15:55:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+const PORT = 8000;
+
+// Routes 
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+//Rest API endpoints
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+    ${users.map(user => `<li>${user.first_name} ${user.last_name}</li>`).join('')}
+    </ul>
+    `;
+    res.send(html);
+});
+
+app.route("/api/users/:id").get((req, res) => {
+    const id = Number(req.params.id);
+const user = users.find((user) => user.id === id);
+return res.json(user);
+})
+.patch((req, res) => {
+    return res.json({status: "pending"});
+})
+.delete((req, res) => {return res.json({status: "pending"});
+});
+
+
+
+
+///app.post('/api/users', (req, res) => {
+///    res.json({status: "pending"});
+///});
+
+///app.patch('/api/users/:id', (req, res) => {
+///    res.json({status: "pending"});
+///});
+
+///app.delete('/api/users/:id', (req, res) => {
+///    res.json({status: "pending"});
+///});
+
+
+app.listen(PORT, () => console.log(`Server started at Port 8000`));
+
+
